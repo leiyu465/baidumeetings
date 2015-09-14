d333d08ef62496a01ae9ffa702fa08dfd059febb
yinyong <yanni4night@gmail.com>
2015-09-15 00:51:05 +0800

    add show my schedule

--- scripts/back.js
+++ scripts/back.js
@@ -10,20 +10,24 @@
  * @since 0.1.0
  */
 
-require(['./lib/jquery', './scripts/meetings'], function ($, Meetings) {
+require(['./lib/jquery', './scripts/meetings'], function($, Meetings) {
     $ = $ || window.$;
 
-   /* chrome.tabs.create({
-        url: 'http://meeting.baidu.com/web/scheduleList'
-    });*/
+    $('.myschedule').click(function(e) {
+        e.preventDefault();
+        chrome.tabs.create({
+            url: 'http://meeting.baidu.com/web/scheduleList'
+        });
+    });
+
     console.debug('start check meetings');
-    
+
     chrome.alarms.create('checkMeetings', {
-        when: Date.now() + 500,
+        when: Date.now() + 1500,
         periodInMinutes: 1
     });
 
-    chrome.alarms.onAlarm.addListener(function (alarm) {
+    chrome.alarms.onAlarm.addListener(function(alarm) {
         Meetings.list();
     });
 });
