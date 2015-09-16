62863058cbbe3f4c36f1f9a474a260989160f5c3
yinyong <yanni4night@gmail.com>
2015-09-16 23:55:03 +0800

    add settings.js

---
+++ scripts/settings.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) 2014 yanni4night.com
+ * settings.js
+ *
+ * changelog
+ * 2015-09-16[23:52:29]:revised
+ *
+ * @author yanni4night@gmail.com
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+define([], function() {
+
+});
