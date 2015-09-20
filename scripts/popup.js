d7e322d29ad1ef58fe5538b81a8c17c4b1633082
yinyong02 <yinyong02@baidu.com>
2015-09-20 10:56:58 +0800

    support transfer

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,6 +5,7 @@
  * changelog
  * 2015-09-15[11:02:07]:revised
  * 2015-09-20[00:04:57]:support cancel/checkin/checkout
+ * 2015-09-20[10:56:45]:support transfer
  *
  * @author yanni4night@gmail.com
  * @version 0.1.1
@@ -13,17 +14,25 @@
 require(['./lib/underscore', './scripts/meetings'], function (_, Meetings) {
     _ = _ || window._;
 
+    // 表格模板
     var tplFunc = _.template($('#scheduleTpl').html());
 
     var Popup = function () {
+        // 缓存
         var scheduleList;
 
         this.start = function () {
             return this.initDomEvents().refreshScheduleTable();
         };
 
+        /**
+         * 刷新列表，重新请求。
+         * 
+         * @return {this}
+         */
         this.refreshScheduleTable = function () {
             Meetings.list(function (err, schedules) {
+                // 先缓存再渲染
                 if (schedules) {
                     scheduleList = schedules
                 }
@@ -35,9 +44,15 @@ require(['./lib/underscore', './scripts/meetings'], function (_, Meetings) {
             return this;
         };
 
+        /**
+         * 根据ID查询缓存中的实例。
+         * 
+         * @param  {string} id 
+         * @return {Object|undefined}    Schedule对象
+         */
         this.findScheduleById = function (id) {
             if (!Array.isArray(scheduleList) || !scheduleList.length) {
-                return null;
+                return undefined;
             }
 
             return scheduleList.filter(function (schedule) {
@@ -45,30 +60,73 @@ require(['./lib/underscore', './scripts/meetings'], function (_, Meetings) {
             })[0];
         };
 
+        /**
+         * 确认对话框。
+         * 
+         * @param  {string}   msg
+         * @param  {Function} cb
+         */
         this.confirm = function (msg, cb) {
             BootstrapDialog.confirm({
                 title: '确认',
                 message: msg,
-                type: BootstrapDialog.TYPE_WARNING, // <-- Default value is BootstrapDialog.TYPE_PRIMARY
-                closable: true, // <-- Default value is false
-                draggable: true, // <-- Default value is false
-                btnCancelLabel: '取消', // <-- Default value is 'Cancel',
-                btnOKLabel: '确认', // <-- Default value is 'OK',
-                btnOKClass: 'btn-warning', // <-- If you didn't specify it, dialog type will be used,
+                closable: true,
+                draggable: true,
+                btnCancelLabel: '取消',
+                btnOKLabel: '确认',
                 callback: $.isFunction(cb) ? cb : $.noop
             });
         };
 
+        /**
+         * 提示对话框。
+         * 
+         * @param  {string}   msg
+         * @param  {Function} cb
+         */
         this.alert = function (msg, cb) {
             BootstrapDialog.alert({
                 title: '通知',
                 message: msg,
-                type: BootstrapDialog.TYPE_INFO, // <-- Default value is BootstrapDialog.TYPE_PRIMARY
-                closable: true, // <-- Default value is false
-                draggable: true, // <-- Default value is false
-                buttonLabel: '确定', // <-- Default value is 'OK',
+                closable: true,
+                draggable: true,
+                buttonLabel: '确定',
                 callback: $.isFunction(cb) ? cb : $.noop
             })
+        };
+
+        /**
+         * 输入对话框。
+         * 
+         * @param  {string}   msg
+         * @param  {Function} cb
+         */
+        this.prompt = function (msg, cb) {
+            BootstrapDialog.show({
+                title: msg,
+                message: '<div class="input-group">\
+          <span class="input-group-btn">\
+            <button class="btn btn-default" type="button">To</button>\
+          </span>\
+          <input type="text" class="form-control" placeholder="transfer@baidu.com">\
+        </div>',
+                buttons: [{
+                    label: '取消',
+                    action: function (dialog) {
+                        dialog.close();
+                        cb();
+                    }
+                }, {
+                    label: '确定',
+                    action: function (dialog) {
+                        var to = dialog.getModalBody().find('input').val().trim();
+                        if (to) {
+                            cb(to);
+                            dialog.close();
+                        }
+                    }
+                }]
+            });
         };
     };
 
@@ -76,6 +134,7 @@ require(['./lib/underscore', './scripts/meetings'], function (_, Meetings) {
         var self = this;
 
         $(document)
+            // 签入按钮事件
             .delegate('.bdm-checkin', 'click', function (e) {
                 e.preventDefault();
 
@@ -100,6 +159,7 @@ require(['./lib/underscore', './scripts/meetings'], function (_, Meetings) {
                     }
                 });
             })
+            // 签出按钮事件
             .delegate('.bdm-checkout', 'click', function (e) {
                 e.preventDefault();
 
@@ -123,6 +183,7 @@ require(['./lib/underscore', './scripts/meetings'], function (_, Meetings) {
                     }
                 });
             })
+            // 取消按钮事件
             .delegate('.bdm-cancel', 'click', function (e) {
                 e.preventDefault();
 
@@ -147,21 +208,34 @@ require(['./lib/underscore', './scripts/meetings'], function (_, Meetings) {
                     }
                 });
             })
+            // 转让按钮事件
             .delegate('.bdm-transfer', 'click', function (e) {
                 e.preventDefault();
-                var id = $(this).data('sid');
-                self.alert('暂不支持转出');
-                if (!id) {
 
+                var schedule;
+                var id = $(this).data('sid');
+
+                if (!id || !(schedule = self.findScheduleById(id))) {
+                    self.alert('出错，没有找到会议信息，请重试');
                     return;
                 }
+
+                self.prompt('请输入转让者的邮箱', function (to) {
+                    if (to) {
+                        schedule.transfer(to, function (err) {
+                            if (!err) {
+                                self.alert('转让给' + to + '成功!');
+                                self.refreshScheduleTable();
+                            } else {
+                                self.alert('转让失败!');
+                            }
+                        });
+                    }
+                });
 
             });
         return this;
     };
 
-
-
     new Popup().start();
-
 });
--- scripts/schedule.js
+++ scripts/schedule.js
@@ -11,6 +11,11 @@
  */
 define([], function () {
 
+    /**
+     * 网络请求。
+     * 
+     * @type {Object}
+     */
     var Web = {
         request: function (url, cb) {
             $.getJSON(url).done(function (data) {
@@ -25,58 +30,62 @@ define([], function () {
         }
     };
 
+    /**
+     * 一个会议室预订信息条目。
+     * 
+     * @param {Object} metaData
+     * @class
+     */
     var Schedule = function (metaData) {
         var id;
-
-        this.getMetaData = function () {
-            return metaData;
-        };
 
         this.get = function (key) {
             return metaData[key];
         };
 
-        this.getDateTimeStr = function () {
-            return [this.get('日期'), '(', this.get('星期'), ')', ' ', this.get('开始时间'), '~', this.get(
-                '结束时间')].join('');
-        };
-
-        this.getLocationStr = function () {
-            return [this.get('地域'), this.get('楼层'), ' ', this.get('会议室描述'), ' ', this.get('会议室名称')].join(
-                '');
-        };
-
-        this.getStatus = function () {
-            return this.get('当前状态');
-        };
-
         this.getId = function () {
             return id || (this.get('操作').match(/\b\d+\b/) || [])[0];
         };
-
-        this.isInPlan = function () {
-            return '未开始' === this.getStatus();
-        };
-
-        this.canCheckin = function () {
-            return !!~['可签入'].indexOf(this.getStatus());
-        };
-
-        this.canTransfer = function () {
-            return !!~['未开始', '可签入'].indexOf(this.getStatus());
-        };
-
-        this.canCancel = function () {
-            return this.canTransfer();
-        };
-
-        this.canCheckout = function () {
-            return !!~['已签入'].indexOf(this.getStatus());
-        };
-
     };
 
     Schedule.prototype = {
+        getDateTimeStr: function () {
+            return [this.get('日期'), '(', this.get('星期'), ')', ' ', this.get('开始时间'), '~', this.get(
+                '结束时间')].join('');
+        },
+
+        getLocationStr: function () {
+            return [this.get('地域'), this.get('楼层'), ' ', this.get('会议室描述'), ' ', this.get('会议室名称')].join(
+                '');
+        },
+
+        getStatus: function () {
+            return this.get('当前状态');
+        },
+        isInPlan: function () {
+            return '未开始' === this.getStatus();
+        },
+        hasCheckedOut: function () {
+            return '已签出' === this.getStatus();
+        },
+        hasFinished: function () {
+            return !!~this.getStatus().indexOf('已结束');
+        },
+        hasOperations: function () {
+            return !(this.hasFinished() || this.hasCheckedOut());
+        },
+        canCheckin: function () {
+            return !!~['可签入'].indexOf(this.getStatus());
+        },
+        canTransfer: function () {
+            return !!~['未开始', '可签入'].indexOf(this.getStatus());
+        },
+        canCancel: function () {
+            return this.canTransfer();
+        },
+        canCheckout: function () {
+            return !!~['已签入'].indexOf(this.getStatus());
+        },
         checkin: function (cb) {
             Web.request('http://meeting.baidu.com/web/checkIn?scheduleId=' + this.getId(), cb);
         },
